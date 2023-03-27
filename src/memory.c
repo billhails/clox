d728dacac3914b87b78039e9fbb9c6505557b64a
Bill Hails <billhails2014@gmail.com>
2023-03-27 16:11:55 +0100

    basic class declaration

--- src/memory.c
+++ src/memory.c
@@ -67,21 +67,29 @@ void blackenObject(Obj *object) {
     printf("\n");
 #endif
     switch (object->type) {
-        case OBJ_CLOSURE:
+        case OBJ_CLASS: {
+            ObjClass *klass = (ObjClass *)object;
+            markObject((Obj *)klass->name);
+            break;
+        }
+        case OBJ_CLOSURE: {
             ObjClosure *closure = (ObjClosure *)object;
             markObject((Obj *)closure->function);
             for (int i = 0; i < closure->upvalueCount; i++) {
                 markObject((Obj *)closure->upvalues[i]);
             }
             break;
-        case OBJ_FUNCTION:
+        }
+        case OBJ_FUNCTION: {
             ObjFunction *function = (ObjFunction *)object;
             markObject((Obj *)function->name);
             markArray(&function->chunk.constants);
             break;
-        case OBJ_UPVALUE:
+        }
+        case OBJ_UPVALUE: {
             markValue(((ObjUpvalue *)object)->closed);
             break;
+        }
         case OBJ_NATIVE:
         case OBJ_STRING:
             break;
@@ -94,6 +102,10 @@ static void freeObject(Obj *object) {
 #endif
 
     switch (object->type) {
+        case OBJ_CLASS: {
+            FREE(ObjClass, object);
+            break;
+        }
         case OBJ_CLOSURE: {
             ObjClosure *closure = (ObjClosure *)object;
             FREE_ARRAY(ObjUpvalue*, closure->upvalues, closure->upvalueCount);
