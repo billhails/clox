38e92d8fb097f9d7d14ab13cab5f7f0297756027
Bill Hails <billhails2014@gmail.com>
2023-03-28 14:14:59 +0100

    method calls and initializers

--- Makefile
+++ Makefile
@@ -14,14 +14,22 @@ clox: $(OBJ)
 
 -include $(DEP)
 
-$(OBJ): obj/%.o: src/%.c
+$(OBJ): obj/%.o: src/%.c | obj
 	$(CC) -c $< -o $@
 
-$(DEP): dep/%.d: src/%.c
+$(DEP): dep/%.d: src/%.c | dep
 	$(CC) -MM -MT $(patsubst dep/%,obj/%,$(patsubst %.d,%.o,$@)) -o $@ $<
+
+dep:
+	mkdir $@
+
+obj:
+	mkdir $@
 
 clean: deps
 	rm -f clox $(OBJ)
 
 deps:
 	rm -f $(DEP)
+
+# vim: set noet,sw=8,tabstop=8
--- src/memory.c
+++ src/memory.c
@@ -67,9 +67,16 @@ void blackenObject(Obj *object) {
     printf("\n");
 #endif
     switch (object->type) {
+        case OBJ_BOUND_METHOD: {
+            ObjBoundMethod *bound = (ObjBoundMethod *)object;
+            markValue(bound->receiver);
+            markObject((Obj *)bound->method);
+            break;
+        }
         case OBJ_CLASS: {
             ObjClass *klass = (ObjClass *)object;
             markObject((Obj *)klass->name);
+            markTable(&klass->methods);
             break;
         }
         case OBJ_CLOSURE: {
@@ -108,7 +115,13 @@ static void freeObject(Obj *object) {
 #endif
 
     switch (object->type) {
+        case OBJ_BOUND_METHOD: {
+            FREE(ObjBoundMethod, object);
+            break;
+        }
         case OBJ_CLASS: {
+            ObjClass *klass = (ObjClass *)object;
+            freeTable(&klass->methods);
             FREE(ObjClass, object);
             break;
         }
@@ -162,6 +175,7 @@ static void markRoots() {
 
     markTable(&vm.globals);
     markCompilerRoots();
+    markObject((Obj *)vm.initString);
 }
 
 static void traceReferences() {
